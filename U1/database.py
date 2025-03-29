c9646995d605c0b851e7bd5bcbd3e4d70434afdd
wling-art <wlingzhenyu@163.com>
2025-03-30 06:51:15 +0800

    🔨 修改对接 koishi 数据库

--- U1/database.py
+++ U1/database.py
@@ -6,15 +6,14 @@ add_model(__name__)
 
 
 class Channel(Model):
-    id = fields.CharField(max_length=255, description="主键", pk=True)
-    platform = fields.CharField(max_length=255, description="主键")
-    flag = fields.BigIntField(unsigned=True, default=0, description="标志")
-    assignee = fields.CharField(max_length=255, description="受让人")
-    guildId = fields.CharField(max_length=255, description="公会ID")
-    locales = fields.TextField(null=True, description="语言环境")
-    permissions = fields.TextField(null=True, description="权限")
-    createdAt = fields.DatetimeField(null=True, description="创建时间")
+    id = fields.CharField(max_length=255, pk=True)
+    platform = fields.CharField(max_length=255)
+    flag = fields.BigIntField(unsigned=True, default=0, null=True)
+    assignee = fields.CharField(max_length=255, null=True)
+    guildId = fields.CharField(max_length=255, null=True)
+    locales = fields.TextField(null=True, default=None)
+    permissions = fields.TextField(null=True, default=None)
+    createdAt = fields.DatetimeField(null=True, default=None)
 
     class Meta:
-        table = "channel"  # 表名
-        unique_together = (("id", "platform"),)  # 确保 (id, platform) 组合唯一
+        unique_together = ("platform", "flag")
